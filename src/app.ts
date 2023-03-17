2afa6637136ad81625694fdeeb448d122deff05f
Strahinja <strahinja.stojadinovic@pmf.edu.rs>
2023-03-17 10:30:31 +0100

    Error handlers

--- src/app.ts
+++ src/app.ts
@@ -12,14 +12,6 @@ app.use("/player", playerRouter);
 
 
 // Error handlers
-// Attach the first Error handling Middleware
-// function defined above (which logs the error)
 app.use(errorLogger);
-
-// Attach the second Error handling Middleware
-// function defined above (which sends back the response)
 app.use(errorResponder);
-
-// Attach the fallback Middleware
-// function which sends back the response for invalid paths)
 app.use(invalidPathHandler);
